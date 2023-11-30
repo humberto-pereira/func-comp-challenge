d1a8752f52b25fefc216bf3a359ccf533517b906
Humberto Pereira <humberto_pereira@live.com>
2023-11-30 17:28:09 +0000

    using css modules with react chalange

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import css from './css/Sidebar.module.css'
 
 const Sidebar = () => {
     return (
-    <div>
-        <a href='#'>My Photos</a>
-        <a href='#'>My Illustrations</a>
-        <a href='#'>My Paintings</a>
+    <div className={css.sidebar}>
+        <a href='#' target='_blank'>My Photos</a>
+        <a href='#' target='_blank'>My Illustrations</a>
+        <a href='#' target='_blank'>My Paintings</a>
     </div>
     )
 }
