a553699e953b0e0322445a2f5b36cd0a9551e10f
Humberto Pereira <humberto_pereira@live.com>
2023-12-06 14:45:17 +0100

    commit

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import css from './css/Sidebar.module.css'
 
+const Navbar = () => {
+    return (
+    <div className={css.NavBarSimple}>
+        <h1>My Gallery</h1>
+        <span>Hello, guest</span>
+        <button>log in</button>
+    </div>
+    )
+}
+
 const Sidebar = () => {
     return (
-    <div className={css.sidebar}>
+    <div className={css.Sidebar}>
         <a href='#' target='_blank'>My Photos</a>
         <a href='#' target='_blank'>My Illustrations</a>
         <a href='#' target='_blank'>My Paintings</a>
@@ -11,4 +21,4 @@ const Sidebar = () => {
     )
 }
 
-export default Sidebar;
+export default Sidebar, Navbar;
